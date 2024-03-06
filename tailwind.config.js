0704717b8f3e7534aa8a80cd68d024b0e4611cb8
Abere Fejiro <aberefejiro@gmail.com>
2024-03-06 23:49:12 +0100

    structured and added styling to component

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,30 @@
 module.exports = {
   content: ["index.html"],
   theme: {
-    extend: {},
+    fontSize: {
+      lg: "clamp(1.125rem, 1vw, 3rem)",
+      sm: "clamp(0.875rem, 1vw, 2.5rem)",
+      xl: "clamp(1.25rem, 1vw, 3.2rem)",
+      xs: "clamp(0.75rem, 1vw, 2rem)"
+    },
+    extend: {
+      colors: {
+        "dark-cyan": "hsl(185, 75%, 39%)",
+        "desaturated-blue": "hsl(229, 23%, 23%)",
+        "dark-grayish-blue": "hsl(227, 10%, 46%)",
+        "dark-gray": "hsl(0, 0%, 59%)",
+      },
+
+      fontFamily: {
+        "primary-font": ["Kumbh Sans", "sans-serif"],
+      },
+
+      screens: {
+        sm: "375px",
+        xl: "1440px",
+      },
+    },
   },
   plugins: [],
-}
+};
 
